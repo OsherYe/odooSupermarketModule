c59d7be73789339fcd7dd33d27a96311e413b937
OSHERYE <osheryeffet137@gmail.com>
2022-11-19 12:58:45 +0200

    second task

--- odooTask/models/cart.py
+++ odooTask/models/cart.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from xmlrpc.client import DateTime, datetime 
 
 class Cart (models.Model):
     _name = 'supermarket.cart'
@@ -7,9 +8,20 @@ class Cart (models.Model):
 
     date = fields.Date(context_today='current time')
     total = fields.Float(compute='_compute_total_amount')
-
+    twice = fields.Boolean(compute='_compute_if_twice')
+    time_in_super = fields.Datetime('YYYY-MM-DD HH:MM:SS')
+    update_time = fields.Datetime('YYYY-MM-DD HH:MM:SS')
+    total_amount = fields.Integer(compute='_compute_total_amount')
+    
     @api.depends('quantity', 'unit_price')
     def _compute_total_amount(self):
         for record in self:
             total = record.quantity * record.unit_price
             record.total = total
+    
+    @api.depends('customer', 'time_in_super')
+    def _compute_if_twice(self):
+        last_occ = self.time_in_super
+        for record in self:
+            if (record.check_out - last_occ < 8 ): return True 
+        return False  
--- odooTask/models/customer.py
+++ odooTask/models/customer.py
@@ -11,19 +11,26 @@ class Customer(models.Model):
     city = fields.Char('City')
     street = fields.Char('Street')
     
-    most_freq_item = fields.Int(compute='_compute_bs')
-    discount_val = fields.Float(compute= '_apply_discount')
-    total = fields.Float(compute ='_apply_discount')
+    most_freq_item = fields.Int(compute='_compute_item_most_freq')
+    discount_val = fields.Float(compute= '_compute_item_most_freq')
+    total = fields.Float(compute ='_compute_item_most_freq')
+    check_out = fields.Datetime('YYYY-MM-DD HH:MM:SS')
     
     ## discount on best seller product
     @api.depends('value', 'discount')
-    def _compute_bs(self):
+    def _compute_item_most_freq(self):
         for record in self:
             the_item = self.env['supermarket.cartItem'].search((['Name', '=', record.name]), order='quantity', limit = '4')
             record.most_freq_item = the_item
         discount = the_item * discount
         the_item.discount_val = discount
         the_item.total = the_item - discount
+        
+    premium_customer = fields.Boolean(compute='_compute_custumers_appear')
+
+    @api.depends('cart.customer', 'cart.twice') #use compute info from other Module table.
+    def _compute_custumers_appear(self):
+       return (self.cart_id.is_twice_aweek)
         
     
     
